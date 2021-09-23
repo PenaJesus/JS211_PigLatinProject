55429d2131097e1126c74c97edac70fa54d998d0
Jesus898 <jesus81398@gmail.com>
2021-09-23 16:59:36 -0500

    final commit

--- main.js
+++ main.js
@@ -12,10 +12,75 @@ const rl = readline.createInterface({
 
 
 const pigLatin = (word) => {
+  // The basic idea of Pig Latin is to take the first letters of a word up to the first vowel, move them to the end of the word, and add 'ay' to the end of it.
+  
+  //get word = car
+  word = word.trim().toLowerCase()
+  let wordArray = word.split('')
+  console.log(wordArray)
+  let vowels = ['a','e','i','o','u']
+  //checking if first position is a vowel
+   if (vowels.indexOf(wordArray[0]) != -1){
+     console.log ("word + 'yay'",word + 'yay')
+    return word + 'yay'
+   } 
+    else {
+      let counter = 0
+      for (let index = 0; index < word.length; index++) {
+        const individualLetter = word[index];
+        if (vowels.indexOf(wordArray[index]) == -1) {
+          counter = counter + 1 
+          console.log(individualLetter)
+          // console.log(wordArray[index])
+        } 
+        else if 
+          (vowels.indexOf(wordArray[index]) != -1) {
+    
+          break;
+         
+        }
+        
+      }
+      console.log(wordArray[counter])
+      let firstPart = word.substring(0,counter)
+      let secondPart = word.substring(counter)
+      console.log(secondPart.concat(firstPart,'ay'))
+      return secondPart.concat(firstPart,'ay')
+      
+    }
+  
+     
+      
+      // let firstPart = word.substring(0,vowelIndex)
+      // let secondPart = word.substring(vowelIndex)
+      // console.log('vowelIndex',vowelIndex)
+      // console.log('firstPart',firstPart)
+      // console.log('secondPart',secondPart)
+      // secondPart.concat(firstPart,'ay')
+      
+      // console.log('**secondPart',secondPart)
+    
+   //find the vowel then push it to the front then delete the old objects and push to the back adding ay
+     
+       
+     
+    
+    
+    
+    // console.log('individualLetter',individualLetter)
+    // console.log('index',index)
+    // console.log(vowels.includes('vowel includes individualLetter',individualLetter))
+  }
+  // if (i=0;)
+  //if our array is true then we need to move the vowel to index[0]
+
+  //move first letters (up to the first vowel) to the back of the word
+  //then add 'ay' tot he end of that word
+
 
   // Your code here
 
-}
+
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
